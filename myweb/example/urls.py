9ffc9a5932a427e46a76c6c14603ecf7077d810f
hyunnjjung <hyunnjjung@gmail.com>
2024-09-13 18:51:42 +0900

    add : 도서 정보 API 만들기

--- myweb/example/urls.py
+++ myweb/example/urls.py
@@ -1,6 +1,21 @@
 from django.urls import path, include
-from .views import HellopAPI
+from .views import HellopAPI, BookAPI, BooksAPI, bookAPI, booksAPI, BookAPIMixins, BooksAPIMixins, BookViewSet
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register('books', BookViewSet)
+
+
+urlpatterns = router.urls
 
 urlpatterns = [
-    path("hello/", HellopAPI)
+    path("hello/", HellopAPI),
+    path("fbv/books/", booksAPI),
+    path("fbv/book/<int:bid>", bookAPI),
+    path("cbv/books/", BooksAPI.as_view),
+    path("cbv/book/<int:bid>", BookAPI.as_view),
+    path("mixin/books/", BooksAPIMixins.as_view()),
+    path("mixin/book/<int:bid>/", BookAPIMixins.as_view())
 ]
+
+
